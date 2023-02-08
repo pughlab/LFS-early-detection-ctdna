44a91d75c27b435117ec1d244b0aeb1bdf4ca460
derekwong90 <78445989+derekwong90@users.noreply.github.com>
2023-02-08 13:00:18 -0500

    Update and rename scripts/figures/Figure3/fragment_distributions.R to scripts/figures/FigureS7/fragment_distributions.R

--- scripts/figures/FigureS7/fragment_distributions.R
+++ scripts/figures/FigureS7/fragment_distributions.R
@@ -7,11 +7,11 @@ library(ggpubr)
 library(gridExtra)
 
 ### Set paths
-path <- "/Users/derekwong/Library/CloudStorage/OneDrive-UHN/Post-Doc/CHARM_Project/LFS/insert_size"
-outdir <- "/Users/derekwong/Google Drive/Post-Doc/CHARM/LFS/LFS_clinical/figures/fragment_distributions"
-healthy_path <- "/Users/derekwong/Library/CloudStorage/OneDrive-UHN/Post-Doc/Healthy_control_cohorts/CHARM_HBC/insert_size"
-samples <- "/Users/derekwong/Library/CloudStorage/OneDrive-UHN/Post-Doc/CHARM_Project/LFS/samples/sample_list.txt"
-source("/Volumes/GoogleDrive/My Drive/Post-Doc/CHARM/LFS/Figures/TP53_griffin/geom_flat_violin.R")
+path <- ""
+outdir <- ""
+healthy_path <- ""
+samples <- "sample_list.txt"
+source("geom_flat_violin.R")
 
 ### Find files
 frequency <- list.files(path, "LFS_fragment_freq.txt", full.names = TRUE)
